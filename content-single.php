70d419595fa573e66ae45e63013709613d8fcc5b
Ilya <ilya.m.polyakov@gmail.com>
2015-08-03 16:25:54 -0400

    Modified css of markup and of single pages. End of 8.3

--- content-single.php
+++ content-single.php
@@ -6,7 +6,6 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
                                     
                                     <?php
                                         /* translators: used between list items, there is a space after the comma */
@@ -17,10 +16,17 @@
                                         }
                                     ?>
                 
-                
+                		<h1 class="entry-title"><?php the_title(); ?></h1>
                 
 		<div class="entry-meta">
 			<?php my_simone_posted_on(); ?>
+                                                       <?php 
+                                                                if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) { 
+                                                                    echo '<span class="comments-link">';
+                                                                    comments_popup_link( __( 'Leave a comment', 'my-simone' ), __( '1 Comment', 'my-simone' ), __( '% Comments', 'my-simone' ) );
+                                                                    echo '</span>';
+                                                                }
+                                                       ?>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
