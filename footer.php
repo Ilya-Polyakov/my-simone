c06b455b988b33b416ecfe9f0bb86f5625e8ca50
Ilya <ilya.m.polyakov@gmail.com>
2015-08-03 13:15:19 -0400

    Add widgetized area to footer. End of 7.1

--- footer.php
+++ footer.php
@@ -11,10 +11,11 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'my-simone' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'my-simone' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'my-simone' ), 'my-simone', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+                                <?php get_sidebar( 'footer' ); ?>
+                                    <div class="site-info">
+                                            <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'my-simone' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'my-simone' ), 'WordPress' ); ?></a>
+                                            <span class="sep"> | </span>
+                                            <?php printf( __( 'Theme: %1$s by %2$s.', 'my-simone' ), 'my-simone', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
