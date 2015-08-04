6efcc544844fa4055743e2f159ff587ab75db910
Ilya <ilya.m.polyakov@gmail.com>
2015-08-04 10:48:28 -0400

    Finished 10.5. Styling featured image. Edited content-single.php and style.css. Includes image-shifter

--- content-single.php
+++ content-single.php
@@ -6,13 +6,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>	
 
-        <?php 
+            <?php 
             if (has_post_thumbnail()) {
                 echo '<div class="single-post-thumbnail clear">';
+                echo '<div class="image-shifter">';
                 echo the_post_thumbnail('large-thumb');
                 echo '</div>';
+                echo '</div>';
             }
-        ?>
+            ?>
+      
 
         <header class="entry-header">
                                     
